66283f50c48cf9e40b808c7cfedb051018e317a0
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-04-14 20:39:33 +0200

    Bonus 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+function cartReducer(addedProducts, action) {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      const addedProduct = addedProducts.find(p => p.name === action.payload.name);
+      if (addedProduct) {
+        action.payload.quantity = addedProduct.quantity + 1;
+      } else {
+        return [...addedProducts, {
+          ...action.payload,
+          quantity: 1
+        }];
+      }
+    case 'UPDATE_QUANTITY':
+      if (action.payload.quantity < 1 || isNaN(action.payload.quantity)) {
+        return addedProducts;
+      }
+      return addedProducts.map(p => p.name === action.payload.name ? { ...p, quantity: action.payload.quantity } : p);
+    case 'REMOVE_ITEM':
+      return addedProducts.filter(p => p.name !== action.payload);
+    default:
+      return state;
+  }
+}
 
 function App() {
 
@@ -10,31 +34,7 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
-  const [addedProducts, setAddedProducts] = useState([]);
-
-  const updateProductQuantity = (name, quantity) => {
-    if (quantity < 1 || isNaN(quantity)) {
-      return;
-    }
-    setAddedProducts(curr =>
-      curr.map(p => p.name === name ? { ...p, quantity } : p))
-  }
-
-  const addToCart = product => {
-    const addedProduct = addedProducts.find(p => p.name === product.name);
-    if (addedProduct) {
-      updateProductQuantity(addedProduct.name, addedProduct.quantity + 1)
-      return;
-    }
-    setAddedProducts(curr => [...curr, {
-      ...product,
-      quantity: 1
-    }])
-  }
-
-  const removeFromCart = product => {
-    setAddedProducts(curr => curr.filter(p => p.name !== product.name));
-  }
+  const [addedProducts, dispatchCart] = useReducer(cartReducer, []);
 
   const totalToPay = addedProducts.reduce((acc, p) => acc + (p.price * p.quantity), 0);
   return (
@@ -44,7 +44,7 @@ function App() {
         {products.map((product, i) => (
           <li key={i}>
             <p>{product.name} ({product.price.toFixed(2)}€)</p>
-            <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+            <button onClick={() => dispatchCart({ type: 'ADD_ITEM', payload: product })}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
@@ -54,10 +54,15 @@ function App() {
           {addedProducts.map((p, i) => (
             <li key={i}>
               <p>
-                <input type="number" value={p.quantity} onChange={e => updateProductQuantity(p.name, parseInt(e.target.value))} />
+                <input type="number" value={p.quantity} onChange={
+                  e => dispatchCart({
+                    type: 'UPDATE_QUANTITY',
+                    payload: { name: p.name, quantity: parseInt(e.target.value) }
+                  })
+                } />
                 <span> x {p.name} ({p.price.toFixed(2)}€)</span>
               </p>
-              <button onClick={() => removeFromCart(p)}>Rimuovi dal carrello</button>
+              <button onClick={() => dispatchCart({ type: 'REMOVE_ITEM', payload: p.name })}>Rimuovi dal carrello</button>
             </li>
           ))}
         </ul>
